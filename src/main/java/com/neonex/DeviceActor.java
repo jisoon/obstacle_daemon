33dc672c5ed0fc81b0072632416eebcae613f68f
jisoonchoi <jisoon0923@gmail.com>
2016-02-23 03:01:22 +0900

    comment add

--- src/main/java/com/neonex/DeviceActor.java
+++ src/main/java/com/neonex/DeviceActor.java
@@ -6,6 +6,8 @@ import com.neonex.dto.EqStatus;
 import com.neonex.dto.EventLog;
 import com.neonex.message.StartMsg;
 import com.neonex.utils.HibernateUtils;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -87,26 +89,36 @@ public class DeviceActor extends UntypedActor {
         try {
             for (EqStatus device : devices) {
                 String deviceModelCode = device.getEqInfo().getEqModel().getModelCode();
-                if (device.getLastCommTime() != null) {
-                    if (thresHold.containsKey(deviceModelCode)) {
-                        // 장비 모델 코드로 미연결 인터벌 값을 조회
-                        int connectionInterval = (Integer) thresHold.get(device.getEqInfo().getEqModel().getModelCode());
 
-                        long dbLastCommTime = Long.parseLong(device.getLastCommTime());
-                        long thresholdLastCommTime = calcConnectionThresHoldTime(connectionInterval);
-                        logger.info("thresholdLastCommTime {} ", thresholdLastCommTime);
-                        logger.info("dbLastCommTime {} ", dbLastCommTime);
+                // 장비의 미연결 임계치가 존재 하면 미연결 여부를 체크 하고
+                // 존재 하지 않는다면 skip 함
+                if (thresHold.containsKey(deviceModelCode)) {
 
-                        // DB 시간과 임계치 시간 비교
-                        if (dbLastCommTime < thresholdLastCommTime) {
-                            logger.info("disconnec target device : {} lastCommTime : {}", device.getEqId(), dbLastCommTime);
+                    // 장비 모델 코드로 미연결 인터벌 값을 조회
+                    int connectionInterval = (Integer) thresHold.get(device.getEqInfo().getEqModel().getModelCode());
 
+                    long deviceLastConnTime = 0L;
+                    if (StringUtils.isNotBlank(device.getLastCommTime())) {
+                        deviceLastConnTime = Long.parseLong(device.getLastCommTime());
+                    }
+                    logger.info(">>>> device last connection time {} ", deviceLastConnTime);
+
+                    long thresholdLastCommTime = calcConnectionThresHoldTime(connectionInterval);
+                    logger.info(">>>> device threshold connection time {} ", thresholdLastCommTime);
+
+                    // DB 시간과 임계치 시간 비교
+                    if (deviceLastConnTime < thresholdLastCommTime) {
+                        logger.info(">>>> device status disconnect");
+
+                        // 이미 미연결 장애가 있다면 skip 처리 해야됨
+                        if(hasDisconnectionEvent(device.getEqId())){
+
+                        }else{
                             updateStatusDisconnect(session, device);
-
                             insertDisconnectEvent(session, device.getEqId());
-
                             disconnectCount++;
                         }
+
                     }
                 }
             }
@@ -119,6 +131,10 @@ public class DeviceActor extends UntypedActor {
         session.getTransaction().commit();
         session.close();
         return disconnectCount;
+    }
+
+    private boolean hasDisconnectionEvent(String eqId) {
+        return false;
     }
 
     /**
